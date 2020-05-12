2de056df1815adfd9c0ab96f4c60181467ad403a
Edmund Kotei <edmundkotei@gmail.com>
2020-05-12 17:41:23 -0400

    completed the see our impact sliders component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImageSlidersComponent } from './image-sliders/image-sliders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageSlidersComponent
   ],
   imports: [
     BrowserModule,
