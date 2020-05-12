da15fe512eb108740285128c18b35b6c8dca31e0
Andrew Oluoch <oluoch3@gmail.com>
2020-05-12 16:52:56 -0400

    submit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OpportunityDivideComponent } from './opportunity-divide/opportunity-divide.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OpportunityDivideComponent
   ],
   imports: [
     BrowserModule,
