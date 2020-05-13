5d0bf96beaeb0e0c8794de2808179ecd001da10c
Edmund Kotei <edmundkotei@gmail.com>
2020-05-13 15:20:53 -0400

    created and edited the how to apply component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImageSlidersComponent } from './image-sliders/image-sliders.component';
+import { HowToApplyComponent } from './how-to-apply/how-to-apply.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImageSlidersComponent
+    ImageSlidersComponent,
+    HowToApplyComponent
   ],
   imports: [
     BrowserModule,
