e8cfc56c02a27b742a23f842230c9fb024ac4c7e
Ondia Brown <Ondianeveah19@gmail.com>
2020-05-14 09:23:47 -0700

    revised headlines component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeadlinesComponent } from './headlines/headlines.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SocialMediaComponent } from './social-media/social-media.component';
 import { NavHeaderComponent } from './nav-header/nav-header.component';
@@ -27,6 +27,7 @@ import { AllNewsRoutingComponent } from './all-news-routing/all-news-routing.com
     OurAwardsRoutingComponent,
     UpcomingEventsRoutingComponent,
     AllNewsRoutingComponent,
+    HeadlinesComponent,
   ],
   imports: [
     BrowserModule,
